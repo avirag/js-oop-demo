766bc47ae697f31765786778eaba612c0b4520fe
Anita Virag <ik.virag@gmail.com>
2019-02-19 15:29:54 +0100

    creating your own prototypical inheritance

---
+++ shape.js
@@ -0,0 +1,35 @@
+function Shape(color) {
+  this.color = color;
+}
+
+Shape.prototype.duplicate = function() {
+  console.log('duplicate');
+}
+
+function Circle(radius, color) {
+  Shape.call(this, color);
+
+  this.radius = radius;
+}
+
+/* Circle.prototype = Object.create(Object.prototype); // objectBase */
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.draw = function() {
+  console.log('draw');
+}
+
+const s = new Shape();
+const c = new Circle(1, 'red');
+
+// new Circle.prototype.constructor(1)
+// equal to
+// new Circle(1)
+
+function Square(size) {
+  this.size = size;
+}
+
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.constructor = Square;
