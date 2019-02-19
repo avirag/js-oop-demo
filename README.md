4ab3c228efd409dabca5f7765475d36ec9211209
Anita Virag <ik.virag@gmail.com>
2019-02-19 12:38:06 +0100

    abstraction

--- README.md
+++ README.md
@@ -27,3 +27,4 @@ new String()
 Primitives are copied by their value
 Objects are copied by their reference
 ```
+* Private properties and methods
--- index.js
+++ index.js
@@ -13,12 +13,35 @@ const circle = createCircle(1);
 // Constructor Function
 function Circle(radius) {
   this.radius = radius;
+
+  let defaultLocation = {
+    x: 0,
+    y: 0
+  };
+
+  this.getDefaultLocation = function() {
+    return defaultLocation;
+  }
+
+  Object.defineProperty(this, 'defaultLocation', {
+    get: function() {
+      return defaultLocation;
+    },
+    set: function(value) {
+      if(!value.x || !value.y) {
+        throw new Error('Invalid location');
+      }
+      defaultLocation = value;
+    }
+  });
+
   this.draw = function() {
     console.log('draw');
   }
 }
 
 const another = new Circle(1); // without 'new' keyword 'this' references the Window object
+// another.defaultLocation = 1;
 
 const Circle1 = new Function('radius', `
 this.radius = radius;
@@ -36,4 +59,16 @@ let x = { value: 10};
 let y = x;
 x.value = 20;
 
- 
+for (let key in circle) {
+  if(typeof circle[key] !== 'function') {
+    console.log(circle[key]);
+  }
+}
+
+const keys = Object.keys(circle);
+console.log(keys);
+
+if('radius' in circle) {
+  console.log('Circle has a radius.');
+}
+
